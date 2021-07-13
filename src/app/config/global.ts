b202d20068d82c587fd96f52a4bc6d1782eb0f6b
steven96SaMo <60241435+steven96SaMo@users.noreply.github.com>
2021-07-12 20:06:50 -0500

    Bug fixed

--- src/app/config/global.ts
+++ src/app/config/global.ts
@@ -2,11 +2,14 @@ export const GlobalVariable = Object.freeze({
     /* Here all the BackEnd routes that will be 
        used for communication are declared */
 
-    //BASE_SERVER_URL: "http://localhost:8000/",
-    BASE_SERVER_URL: "https://ambienetuq.tk/",
+    // PHP
     BASE_API_URL_PHP: "https://ambienet.000webhostapp.com/api/",
     PROFILE_PICTURE_PHP: "attachFile.php",
     POST_PICTURE_PHP: "attachFile.php",
+
+    // DJANGO
+    //BASE_SERVER_URL: "http://localhost:8000/",
+    BASE_SERVER_URL: "https://ambienetuq.tk/",
     REGISTER_USER: "users/signup/",
     LOGIN_USER: "users/login/",
     READ_USERS: "users/",
